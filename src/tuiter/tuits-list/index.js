10e9183230d85352a82da264969565f153969ec1
AiyappaND <vinayaiyappa24816@gmail.com>
2023-04-02 22:29:24 -0400

    Add ignore rule to empty dependency list

--- src/tuiter/tuits-list/index.js
+++ src/tuiter/tuits-list/index.js
@@ -11,7 +11,8 @@ const TuitsList = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(findTuitsThunk())
-    })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
     return(
         <div>
             <WhatsHappening/>
