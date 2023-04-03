f1a05125b3ac1c8a0584dc651e85810820c5c3cf
AiyappaND <vinayaiyappa24816@gmail.com>
2023-04-02 21:19:50 -0400

    Convert tuit crud ops to server based

---
+++ src/tuiter/tuits-list/index.js
@@ -0,0 +1,35 @@
+import React, {useEffect} from "react";
+import TuitItem from "./tuit-item"
+import {useDispatch, useSelector} from "react-redux";
+import {findTuitsThunk}
+    from "../../services/tuits-thunks";
+import WhatsHappening from "./whats-happening";
+
+const TuitsList = () => {
+    const {tuits, loading} = useSelector(
+        state => state.tuitsData)
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(findTuitsThunk())
+    }, [])
+    return(
+        <div>
+            <WhatsHappening/>
+            <ul className="list-group">
+                {
+                    loading &&
+                    <li className="list-group-item">
+                        Loading...
+                    </li>
+                }
+
+                {
+                    tuits.map(tuit =>
+                        <TuitItem
+                            key={tuit._id} tuit={tuit}/> )
+                }
+            </ul>
+        </div>
+    );
+};
+export default TuitsList;
